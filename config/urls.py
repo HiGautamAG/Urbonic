99a90bceb37488052548610948a9feec0ad3d295
Zaid Kamil <xaidmetamorphos@gmail.com>
2024-04-04 15:31:53 +0530

    models and database

--- config/urls.py
+++ config/urls.py
@@ -4,12 +4,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from shop.views import contact_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
     path('about', TemplateView.as_view(template_name='about.html'), name='about'),
     path('pricing', TemplateView.as_view(template_name='pricing.html'), name='pricing'),
+    path('contact', contact_view, name='contact'),
 ]
 
 if settings.DEBUG:
